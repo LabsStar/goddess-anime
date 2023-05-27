1197812c42b554f41cfabc56a82aa7c0a06b2824
kiyolol <predevxyzlol@gmail.com>
2023-05-26 23:02:21 -0400

    Change things around

--- src/config.ts
+++ src/config.ts
@@ -10,15 +10,15 @@ const config = {
     prefix: "/",
     link: {
         name: "link",
-        id: "1109876060524380260" || process.env.LINK_ID,
+        id: process.env.LINK_ID,
     },
     catch: {
         name: "catch",
-        id: "1109951147898318889" || process.env.CATCH_ID,
+        id: process.env.CATCH_ID,
     },
     help: {
         name: "help",
-        id: "1109866175413878876" || process.env.HELP_ID,
+        id: process.env.HELP_ID,
     },
     TIME_TO_DELETE: 60000,
     SHOP_EXPIRE_DAYS: 3,
@@ -27,7 +27,7 @@ const config = {
     VERSION: version,
     allow_developer_applications: true,
     DEVELOPER_PREFIX: "dev/",
-    COMMUNITY_UPDATES_CHANNEL: "1110434165670821948" || process.env.COMMUNITY_UPDATES_CHANNEL,
+    COMMUNITY_UPDATES_CHANNEL: process.env.COMMUNITY_UPDATES_CHANNEL,
     IS_IN_DEV_MODE: true,
     BOT_ID: process.env.BOT_ID,
     COINS: {
