8512cc477dcc584538bd0ee9390fce911b044685
Stella • Bunny Girl <live.hylia@outlook.com>
2023-06-24 18:05:58 -0400

    Update interactionCreate.ts

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -22,15 +22,15 @@ module.exports = {
     // if (!userDoc) if (command?.hasToBeLinked) return interaction.reply({ content: `You don't have an account yet! Please run </${config.link.name}:${config.link.id}> to create one.`, ephemeral: true });
 
     try {
-      const embed = new MessageEmbed()
-        .setTitle(`Project Shutdown...`)
-        .setURL("https://www.hyperstar.cloud/blog/the-end-of-goddess")
-        .setDescription(`It is with a heavy heart that we announce the closure of our beloved project, Goddess Anime. After an incredible journey filled with excitement and innovation, we have reached a point where we must bid our final farewell. The decision to close the project has been made due to a significant drop in user engagement and interactions.\n\n[Blog Post](https://www.hyperstar.cloud/blog/the-end-of-goddess)`)
-        .setColor("RED")
-        .setImage("https://media.discordapp.net/attachments/1020761875979452457/1120253435758002176/image.png?width=484&height=363")
-        .setTimestamp();
-      await interaction.reply({ embeds: [embed], ephemeral: true });
-      // await command?.execute(interaction as CommandInteraction);
+      // const embed = new MessageEmbed()
+      //   .setTitle(`Project Shutdown...`)
+      //   .setURL("https://www.hyperstar.cloud/blog/the-end-of-goddess")
+      //   .setDescription(`It is with a heavy heart that we announce the closure of our beloved project, Goddess Anime. After an incredible journey filled with excitement and innovation, we have reached a point where we must bid our final farewell. The decision to close the project has been made due to a significant drop in user engagement and interactions.\n\n[Blog Post](https://www.hyperstar.cloud/blog/the-end-of-goddess)`)
+      //   .setColor("RED")
+      //   .setImage("https://media.discordapp.net/attachments/1020761875979452457/1120253435758002176/image.png?width=484&height=363")
+      //   .setTimestamp();
+      // await interaction.reply({ embeds: [embed], ephemeral: true });
+      await command?.execute(interaction as CommandInteraction);
     } catch (error) {
       console.error(error);
 
