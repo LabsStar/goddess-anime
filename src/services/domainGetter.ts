fb9621a51faf83f3319f2b9346cb68b75b1f6a45
Stella <live.hylia@outlook.com>
2023-07-07 20:31:57 -0400

    Update domainGetter.ts
    Signed-off-by: Stella <live.hylia@outlook.com>

--- src/services/domainGetter.ts
+++ src/services/domainGetter.ts
@@ -1,38 +1,21 @@
 import axios from "axios";
 
 const isDev = process.env.node_mode;
-
-const getEndpoint = () => {
-    if (isDev == "dev") return "http://localhost/api";
-    else return "https://goddessanime.com/api";
-}
+const getEndpoint = () => (isDev === "dev" ? "http://localhost/api" : "https://goddessanime.com/api");
 
 async function getuser(u: string, host: string, req: any, res: any) {
+  if (!u) return res.redirect(getEndpoint());
 
-    if (!u) return res.redirect(getEndpoint());
+  try {
+    const { data } = await axios.get(`${getEndpoint()}/social-name/${u}`);
 
-    console.log(u)
+    if (data.error) return res.redirect(getEndpoint().replace("/api", ""));
+    if (data.data.domain !== host) return res.redirect(getEndpoint().replace("/api", ""));
 
-    try {
-        const { data } = await axios.get(`${getEndpoint()}/social-name/${u}`);
-
-        console.log(data)
-
-        if (data.error) return res.redirect(getEndpoint().replace("/api", ""));
-
-        if (data.data.domain !== host) return res.redirect(getEndpoint().replace("/api", ""));
-
-        return res.redirect(`${getEndpoint().replace("/api", "")}/user/${data.data.id}`)
-    } catch (error) {
-        return res.status(500).json({
-            error: (error as Error).message,
-            status: 404,
-            params: req.params,
-            query: req.query
-        })
-    }
-    
+    return res.redirect(`${getEndpoint().replace("/api", "")}/user/${data.data.id}`);
+  } catch (error) {
+     return res.redirect(`${getEndpoint().replace("/api", "/error")}`);
+  }
 }
 
-
-export default getuser
+export default getuser;
