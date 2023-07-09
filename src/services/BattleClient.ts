d33ac0fc1efe819e018c9d361f0ecad196fdabc9
Stella <live.hylia@outlook.com>
2023-07-09 17:49:59 -0400

    Change Links and Ttitle

--- src/services/BattleClient.ts
+++ src/services/BattleClient.ts
@@ -20,13 +20,13 @@ export default class BattleClient {
 
     async startBattle(interaction: any, user1: User, user2: User) {
         const soon_embed = new MessageEmbed()
-            .setTitle(`Battle between ${user1.username} and ${user2.username}`)
+            .setTitle(`Battle Feature`)
             .setAuthor({
                 name: `${interaction.client.users.cache.get(config.ownerID)?.username}`,
                 iconURL: `${interaction.client.users.cache.get(config.ownerID)?.displayAvatarURL({ dynamic: true })}`,
                 url: `https://goddessanime.com/user/${config.ownerID}`
             })
-            .setURL("https://github.com/LabsStar/goddess-anime/blob/main/src/services/activity.ts")
+            .setURL("https://github.com/LabsStar/goddess-anime/blob/main/src/services/BattleClient.ts")
             .setDescription("Sorry but the **battle** feature is not available yet!\n\nIf you want to help us develop this feature, please view our `src/services/BattleClient.ts` file on our GitHub repository. Thank you!\n\n[GitHub Repository](https://github.com/LabsStar/goddess-anime/blob/main/src/services/BattleClient.ts)")
             .setColor("RED")
             .setImage("https://contrib.rocks/image?repo=LabsStar/goddess-anime");
